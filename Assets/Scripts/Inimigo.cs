d12da22086a94506be8ed2d0dbd4ebf05781a889
Pedro Savino <pedrosavinooliveira@gmail.com>
2015-05-24 18:17:55 -0300

    Domingo - 24/05

--- Assets/Scripts/Inimigo.cs
+++ Assets/Scripts/Inimigo.cs
@@ -5,9 +5,16 @@ public abstract class Inimigo : MonoBehaviour {
 	public float vel, velAtk;
 	public float vida;
 	public int dano;
+	public Animator anim;
 
 	public abstract IEnumerator Ataque();
 	public abstract void Dano(float d);
+
+	public void Morte ()
+	{
+		Destroy(gameObject);
+	}
+
 	public abstract void AplicarDebuff(EnumDebuffInimigo debuff, float porcDebuff);
 
 	public enum EnumDebuffInimigo
--- Assets/Scripts/InimigoMelee.cs
+++ Assets/Scripts/InimigoMelee.cs
@@ -11,6 +11,7 @@ public class InimigoMelee : Inimigo {
 	void Start () 
 	{
 		GetComponent<Rigidbody2D>().velocity = new Vector2(-vel,0);
+		anim = GetComponent<Animator>();
 	}
 
 	override public IEnumerator Ataque()
@@ -46,9 +47,21 @@ public class InimigoMelee : Inimigo {
 	{
 		vida-=d;
 
+
+
 		if(vida<=0)
-			Destroy(gameObject);
+		{
+			GetComponent<Collider2D>().enabled =false;
+			GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+			anim.Play("Morte",0,0);
+
+		}
+
+		else
+			anim.Play("Dano",1,0);
 	}
+
+
 
 	override public void AplicarDebuff(EnumDebuffInimigo debuff, float porcDebuff)
 	{
